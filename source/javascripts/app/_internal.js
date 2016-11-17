214893bb661e30551ecf26f5906f63c4213d0c1c
Scott Robertson <scott@scottusrobus.com>
2016-11-17 07:10:34 -0800

    remove console.log

--- source/javascripts/app/_internal.js
+++ source/javascripts/app/_internal.js
@@ -1,7 +1,6 @@
 (function(window, $) {
   $(document).on('ready', function() {
     if (window.location.search.match(/internal\=1/)) {
-      console.log("boom");
       $('body').addClass('show-internal');
     }
   });
