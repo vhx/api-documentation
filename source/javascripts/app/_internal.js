b5b275cbb28e6cbdf81f1b9f79666b2e9763b323
Joe O'Conor <joe.oconor@vimeo.com>
2023-10-11 15:46:40 -0400

    delete source files and build tooling

--- source/javascripts/app/_internal.js
+++
@@ -1,7 +0,0 @@
-(function(window, $) {
-  $(document).on('ready', function() {
-    if (window.location.search.match(/internal\=1/)) {
-      $('body').addClass('show-internal');
-    }
-  });
-})(window, $);
